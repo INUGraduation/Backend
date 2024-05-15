f9416688053809299070282ad3b072d3d1c5d63c
sunghyun <20190171@inu.ac.kr>
2024-05-15 22:27:05 +0900

    first commit

---
+++ src/test/java/com/example/inu/InuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
