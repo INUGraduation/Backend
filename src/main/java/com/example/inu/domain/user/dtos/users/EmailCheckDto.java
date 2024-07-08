e72b360411a9089f284ae63dd4630fd833f81598
sunghyun <20190171@inu.ac.kr>
2024-07-08 10:55:52 +0900

    feat : 이메일 중복체크 dto생성

---
+++ src/main/java/com/example/inu/domain/user/dtos/users/EmailCheckDto.java
@@ -0,0 +1,10 @@
+package com.example.inu.domain.user.dtos.users;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class EmailCheckDto {
+    private String email;
+}
