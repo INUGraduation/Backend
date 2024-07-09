e4eccdfc38dd761436923210b3e0b7ed59a3eed8
sunghyun <20190171@inu.ac.kr>
2024-07-09 14:09:26 +0900

    UserPosition 엔티티 생성

---
+++ src/main/java/com/example/inu/domain/user/entities/UserPosition.java
@@ -0,0 +1,30 @@
+package com.example.inu.domain.user.entities;
+
+
+import com.example.Backend.domain.common.entities.Position;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_positions")
+public class UserPosition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id" , nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "position_id", nullable = false)
+    private Position position;
+}
+//User와 Position의 중간 테이블
