39b5dccd125230868f5ed657fe05a50f68fd2a76
sunghyun <20190171@inu.ac.kr>
2024-07-09 14:45:28 +0900

    TechStack entity구현

---
+++ src/main/java/com/example/inu/domain/user/entities/TechStack.java
@@ -0,0 +1,30 @@
+package com.example.inu.domain.user.entities;
+
+
+import com.example.inu.domain.common.entities.Position;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "techStacks")
+public class TechStack {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "position_id", nullable = false)
+    private Position position;
+
+    @Column(nullable = false)
+    private String name;
+}
+//왜 안되는거지
