bc81dd41f1e0943ca066743abfecc75de74f8a69
sunghyun <20190171@inu.ac.kr>
2024-07-15 13:26:54 +0900

    faet 게시글 생성 dto

--- src/main/java/com/example/inu/domain/recruitments/dtos/RecruitmentCreateDto.java
+++ src/main/java/com/example/inu/domain/recruitments/dtos/RecruitmentCreateDto.java
@@ -1,2 +1,23 @@
-package com.example.inu.domain.recruitments.dtos;public class RecruitmentCreateDto {
+package com.example.inu.domain.recruitments.dtos;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+public class RecruitmentCreateDto {
+    private String type;
+    private String title;
+    private Integer number;
+    private Date startDate;
+    private Date endDate;
+    private Date deadline;
+    private String introduction;
+
+    private List<Long> positionIds;    // 포지션 ID 목록
+    private List<Long> techStackIds;  // 테크스택 ID 목록
 }
