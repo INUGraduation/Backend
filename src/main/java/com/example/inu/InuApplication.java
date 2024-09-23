963245d2ddd9c429b1e5b5f2369c32b46c37cf2f
sunghyun <20190171@inu.ac.kr>
2024-09-23 11:15:54 +0900

    feat : 실시간 시간반영

--- src/main/java/com/example/inu/InuApplication.java
+++ src/main/java/com/example/inu/InuApplication.java
@@ -2,8 +2,10 @@ package com.example.inu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing  // JPA Auditing 활성화
 public class InuApplication {
 
 	public static void main(String[] args) {
